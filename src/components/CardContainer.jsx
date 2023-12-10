837bee25ac76f9541799f63b3d1a3ebe83a05f7d
kiymettt <leylakkdemir@gmail.com>
2023-12-10 16:19:36 +0100

    catched input value

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -5,27 +5,36 @@ import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import {data} from "../helpers/data"
 import PlayerCard from "./PlayerCard"
+import {useState} from 'react';
 
 const CardContainer = () => {
+
+    const[search, setSearch] = useState("");
+    console.log(search)
     // console.log(data)
     // const showImage =false
    
   return (
-    <Container className = "rounded-4 my-4 p-3 card-container">
-         <Form.Control className ="w-50 m-auto"
+    <>
+     <Form.Control className ="w-50 m-auto"
           placeholder="Search a player.."
+        //   onChange= {(e) => console.log(e.target.value)}
+        onChange= {(e) => setSearch(e.target.value)}
         />
-        <Row className ="justify-content-center g-3">
-            {data.map((data)=>(
-                <Col xl={3} lg={4} md={6}>
-                {/* <PlayerCard {...data} showImage= {showImage}/> */}
-                <PlayerCard {...data} />
-                </Col>
-              
-            ))}
-            
-        </Row>
-    </Container>
+         <Container className = "rounded-4 my-4 p-3 card-container">
+            <Row className ="justify-content-center g-3">
+                {data.map((data)=>(
+                    <Col xl={3} lg={4} md={6}>
+                    {/* <PlayerCard {...data} showImage= {showImage}/> */}
+                    <PlayerCard {...data} />
+                    </Col>
+                
+                ))}
+                
+            </Row>
+        </Container>
+    </>
+   
   )
 }
 
