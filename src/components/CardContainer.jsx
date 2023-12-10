06f648a1d34a57de2e53d9ed26c0b23e085a86ee
kiymettt <leylakkdemir@gmail.com>
2023-12-10 16:48:34 +0100

    added a new component for Form

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import  Row  from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
 import {data} from "../helpers/data"
 import PlayerCard from "./PlayerCard"
+import FormComp from "./FormComp"
 import {useState} from 'react';
 
 const CardContainer = () => {
@@ -20,14 +20,9 @@ const CardContainer = () => {
    const filteredData = data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase().trim()));
 
   return (
-    <>
-     <Form.Control className ="w-50 m-auto"
-          placeholder="Search a player.."
-        //   onChange= {(e) => console.log(e.target.value)}
-        onChange= {handleChange}
-        type="search"
-        />
-         <Container className = "rounded-4 my-4 p-3 card-container">
+    <>  
+        <FormComp handleChange ={handleChange} />
+        <Container className = "rounded-4 my-4 p-3 card-container">
             <Row className ="justify-content-center g-3">
                 {filteredData.map((data)=>(
                     <Col xl={3} lg={4} md={6}>
--- src/components/FormComp.jsx
+++ src/components/FormComp.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
 
-const FormComp = () =>{
+const FormComp = ({handleChange}) =>{
 
     return(
         <>
-        
+        <Form.Control className ="w-50 m-auto"
+        placeholder="Search a player.."
+        //   onChange= {(e) => console.log(e.target.value)}
+        onChange= {handleChange}
+        type="search"
+        />
         </>
     )
 
